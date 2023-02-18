1221d3b605d61e0a36029beec9f13f22214045f2
AsifKamalTias <asifkamaltias@gmail.com>
2023-02-18 23:25:41 +0600

    fir

--- src/pages/User/RegisterFIR.js
+++ src/pages/User/RegisterFIR.js
@@ -1,31 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import UserHeader from "../../components/user/UserHeader";
 import {Button} from 'react-bootstrap';
 
 const RegisterFIR = () => {
     const [address, setAddress] = useState('');
+    const [station, setStation] = useState('');
     const [offence, setOffence] = useState('');
     const [offenceDescription, setOffenceDescription] = useState('');
-    const [station, setStation] = useState('');
     const [suspect, setSuspect] = useState('');
     const [evidence, setEvidence] = useState('');
 
+    const [formError, setFormError] = useState('');
 
-    const [addressError , setAddressError] = useState('');
-    const [offenceError , setOffenceError] = useState('');
-    const [offenceDescriptionError , setOffenceDescriptionError] = useState('');
-    const [stationError , setStationError] = useState('');
+    const [thanas, setThanas] = useState();
+    const [firTypes, setFirTypes] = useState();
+
+    useEffect(() => {
+        document.title = 'Register FIR | Blockchain-Based Police Complaint and Crime Evidence Management System';
+        axios.get('http://127.0.0.1:3100/api/firs/create')
+            .then(
+                (success) => {
+                    setThanas(success.data.thanas);
+                    setFirTypes(success.data.firTypes);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            )
+    }, []);
 
     const registerFir = (e) =>{
         e.preventDefault();
-        if(address !== '' || address !== null && offenceDescription !== '' || offenceDescription !== null)
+        if(address !== '' || address !== null || offence !== '' || offence !== null || offenceDescription !== '' || offenceDescription !== null || station !== '' || station !== null)
         {
             const data = {
+                userId : localStorage.getItem('loggedUser'),
                 address : address,
-                offenceDescription: offenceDescription
+                station : station,
+                offence : offence,
+                offenceDescription: offenceDescription,
+                suspect: suspect,
+                evidence: evidence
             }
-            axios.post('http://127.0.0.1:3100/api/users/create', data)
+            axios.post('http://127.0.0.1:3100/api/firs/store', data)
             .then(
                 (success)=>{
                     console.log(success);
@@ -36,8 +54,7 @@ const RegisterFIR = () => {
             )
         }
         else{
-            setAddressError('Address is required!');
-            setOffenceDescriptionError('Offence Description is required!');
+            setFormError('Please fill properly');
         }
         
     }
@@ -49,48 +66,47 @@ const RegisterFIR = () => {
                 <div className="col-sm-4">
                     <div className="card p-4">
                     <h2 className="align-items-center text-center text-primary ">FIR Registration</h2>
+                        {formError !== '' ? <div className="alert alert-danger" role="alert">{formError}</div> : ''}
+                        <form method="POST" onSubmit={registerFir} encType="multipart/form-data">
                         <div className="mb-3">
-                            <label htmlFor="address" className="form-label">Address</label>
+                            <label htmlFor="address" className="form-label">Your Address <sup className="text-danger">*</sup></label>
                             <input type="text" name="address" id="address" value={address} onChange={(e)=>{setAddress(e.target.value)}} className="form-control"  placeholder="Enter Address" />
-                             {addressError !== '' || addressError !== null ? addressError : ''}
                         </div>
 
                         <div className="mb-3">
-                            <label htmlFor="offence" className="form-label">Offence</label>
-                            <select className="form-control" name="offence"  id="offence" onChange={(e)=>{setOffence(e.target.value)}} >
-                                <option>Theft</option>
-                                <option>Violence</option>
-                                <option>Cyber Crime</option>
-                                <option>Drug</option>
+                            <label htmlFor="station" className="form-label">Select Police Station <sup className="text-danger">*</sup></label>
+                            <select className="form-select" name="station"  id="station" value={station} onChange={(e)=>{setStation(e.target.value)}} >
+                                <option>Select Station</option>
+                                {thanas !== undefined ? thanas.map((thana, index) => {
+                                    return <option key={index} value={thana._id}>{thana.thanaName}</option>
+                                }) : ''}
                             </select>
-                             {offenceError !== '' || offenceError !== null ? offenceError : ''}
                         </div>
 
                         <div className="mb-3">
-                            <label htmlFor="offenceDescription" className="form-label">Offence Description</label>
+                            <label htmlFor="offence" className="form-label">Offence Type <sup className="text-danger">*</sup></label>
+                            <select className="form-select" name="offence"  id="offence" value={offence} onChange={(e)=>{setOffence(e.target.value)}} >
+                                <option>Select Offence</option>
+                                {firTypes !== undefined ? firTypes.map((firType, index) => {
+                                    return <option key={index} value={firType._id}>{firType.type}</option>
+                                }) : ''}
+                            </select>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="offenceDescription" className="form-label">Offence Description <sup className="text-danger">*</sup></label>
                             <textarea name="offenceDescription" id="offenceDescription" value={offenceDescription} onChange={(e)=>{setOffenceDescription(e.target.value)}} className="form-control"  placeholder="Enter Description" ></textarea>
-                             {offenceDescriptionError !== '' || offenceDescriptionError !== null ? offenceDescriptionError : ''}
                         </div>
 
-                        <div className="mb-3">
-                            <label htmlFor="station" className="form-label">Police Station</label>
-                            <select className="form-control" name="station"  id="station" onChange={(e)=>{setStation(e.target.value)}} >
-                                <option>Kotwali</option>
-                                <option>Mirpur</option>
-                                <option>Uttara</option>
-                                <option>Vatara</option>
-                            </select>
-                             {stationError !== '' || stationError !== null ? stationError : ''}
-                        </div>
 
                         <div className="mb-3">
                             <label htmlFor="suspect" className="form-label">Suspect Information</label>
-                            <input type="text" name="suspect" id="suspect" value={address} onChange={(e)=>{setSuspect(e.target.value)}} className="form-control"  placeholder="Enter Suspect Information" />
+                            <textarea type="text" name="suspect" id="suspect" value={suspect} onChange={(e)=>{setSuspect(e.target.value)}} className="form-control"  placeholder="Enter Suspect Information" ></textarea>
                         </div>
 
                         <div className="mb-3">
-                            <label htmlFor="evidence" className="form-label text-center">Evidence</label>
-                            <input type="file" name="evidence" id="evidence" value={evidence} onChange={(e)=>{setEvidence(e.target.value)}} className="form-control"  placeholder="Upload Evidence" />
+                            <label htmlFor="evidence" className="form-label text-center">Upload Evidences</label>
+                            <input type="file" name="evidence" id="evidence" value={evidence} onChange={(e)=>{setEvidence(e.target.value)}} className="form-control"  placeholder="Upload Evidence" multiple/>
                         </div>
 
                         <div className="d-grid">
@@ -98,6 +114,7 @@ const RegisterFIR = () => {
                             Submit
                             </Button>
                         </div>
+                        </form>
                     </div>
                 </div>
             </div>
